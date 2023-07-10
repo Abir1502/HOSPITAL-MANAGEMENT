1d53bcc56c9b5b5c6553ba2023ac3dcfc2dd8623
Abir1502 <abirbanerjee1502@gmail.com>
2023-07-11 00:35:10 +0530

    Usage of getter and setter

---
+++ SetValue.java
@@ -0,0 +1,44 @@
+
+     class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+public class SetValue {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setName("Abir Banerjee");
+        person.setAge(25);
+        person.setAddress("Asansol, West Bengal");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
