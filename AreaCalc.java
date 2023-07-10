2f8c476a1f09bc6aa8f6e5394c2aac02789a8413
Abir1502 <abirbanerjee1502@gmail.com>
2023-07-11 00:18:09 +0530

    Demonstrate polymorphism by passing instances of different sub-classes to a method

---
+++ AreaCalc.java
@@ -0,0 +1,69 @@
+
+     abstract class Shape {
+    public abstract double calculateArea();
+      }
+
+    class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+class ShapeCalculator {
+    public void printArea(Shape shape) {
+        double area = shape.calculateArea();
+        System.out.println("The area of the shape is: " + area);
+    }
+}
+
+public class AreaCalc {
+    public static void main(String[] args) {
+        ShapeCalculator calculator = new ShapeCalculator();
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        calculator.printArea(rectangle); 
+
+        Circle circle = new Circle(3);
+        calculator.printArea(circle); 
+
+        Triangle triangle = new Triangle(4, 6);
+        calculator.printArea(triangle); 
+    }
+}
